43dd8325ed86d590eb55791c7a46d5c2fd3533c8
Leinerke Suarez <leinerke2000@gmail.com>
2022-03-30 23:28:28 -0500

    visibilitychange.

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
 	constructor() {
 		this.threshold = 0.25;
 		this.handleIntersection = this.handleIntersection.bind(this);
+		this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
 	}
 
 	run(player) {
@@ -11,12 +12,22 @@ class AutoPause {
 		});
 
 		observer.observe(player.media);
+
+		document.addEventListener('visibilitychange', this.handleVisibilityChange);
 	}
 
 	handleIntersection(entries) {
 		const entry = entries[0];
-
 		if (entry.intersectionRatio >= this.threshold) {
+			this.player.play();
+		} else {
+			this.player.pause();
+		}
+	}
+
+	handleVisibilityChange() {
+		const isVisible = document.visibilityState === 'visible';
+		if (isVisible) {
 			this.player.play();
 		} else {
 			this.player.pause();
